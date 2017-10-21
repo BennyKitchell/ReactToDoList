9d8f9deca67bfe53f02b22031546a9ad70eb8912
Benny Kitchell <bennykitchell@gmail.com>
2017-10-20 20:59:17 -0500

    Add the first stages of having a dynamic template using objects

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,15 +1,35 @@
-"use strict";
+'use strict';
 
 console.log("app js is running");
 
-//Begin JSX (Javascript XML)
+//Because userName is a javascript expression, it can call methods and concat strings dynamically
+var app = {
+    title: 'React To-Do List',
+    subtitle: 'This is a paragraph test',
+    number: '2017'
+};
+//This template is the beginning of having a template that references variables
+//instead of referencing static text 
 var template = React.createElement(
-  "h1",
-  null,
-  "React To-Do List"
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'h2',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.number
+    )
 );
 var appRoot = document.getElementById('app');
-
 //Renders the application using two parameters (JSX youd like to render, DOM element)
 //DOM element can be found in the index html with the same id
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 console.log("app js is running");
 
-//Begin JSX (Javascript XML)
-var template = <h1>React To-Do List</h1>;
+//Because userName is a javascript expression, it can call methods and concat strings dynamically
+var app = {
+    title: 'React To-Do List',
+    subtitle: 'This is a paragraph test',
+    number: '2017'
+};
+//This template is the beginning of having a template that references variables
+//instead of referencing static text 
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        <h2>{app.subtitle}</h2>
+        <p>{app.number}</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
-
 //Renders the application using two parameters (JSX youd like to render, DOM element)
 //DOM element can be found in the index html with the same id
 ReactDOM.render(template, appRoot);
