9a0883dd16dc18a6c13d3515b5bc2b88fcc450ce
Benny Kitchell <bennykitchell@gmail.com>
2017-10-23 21:27:50 -0500

    general practics with sublclasses

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,10 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 //Understanding classes by expirementing
@@ -34,12 +38,49 @@ var Person = function () {
     return Person;
 }();
 
-var me = new Person('ben', 10);
+//subclass practice
+
+
+var Student = function (_Person) {
+    _inherits(Student, _Person);
+
+    function Student(name, age) {
+        var major = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'undecided';
+
+        _classCallCheck(this, Student);
+
+        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
+
+        _this.major = major;
+        return _this;
+    }
+
+    _createClass(Student, [{
+        key: 'hasMajor',
+        value: function hasMajor() {
+            if (this.major !== 'undecided') {
+                return true;
+            }
+            return false;
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return this.name + ' is ' + this.age + ' year(s) old, their major is ' + this.major;
+        }
+    }]);
+
+    return Student;
+}(Person);
+
+var me = new Student('ben', 10, 'biology');
 console.log(me.getGreeting());
 console.log(me.getDescription());
+console.log(me.hasMajor());
 console.log(me);
 
-var other = new Person();
+var other = new Student();
 console.log(other.getGreeting());
 console.log(other.getDescription());
+console.log(other.hasMajor());
 console.log(other);
