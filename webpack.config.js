7a73fdc39720a4246911f60478411b51711aedc8
Benny Kitchell <bennykitchell@gmail.com>
2018-01-13 00:00:46 -0600

    Add config file and do basic test to ensure it is working as intended

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// entry point -> output file
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        // absolute path
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+};
